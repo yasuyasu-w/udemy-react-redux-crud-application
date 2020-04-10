aa8879f8b87591b58bdd75c7bac575c1d31564bb
yasuyasu-w <ya.su.ya.su.w0612@gmail.com>
2020-04-10 07:08:27 +0000

    learn props of compo

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
 import React from 'react';
 
-//class App extends Component {
-//  render(){
-//  return (
-//
-//    <React.Fragment>
-//      <label htmlFor="bar">bar</label>
-//
-//    <input type="text" onChange={()=>{console.log("I am clicked.")}} />
-//    </React.Fragment>
-//      
-//  )
-//  }
-//}
-
 const App=()=>{
+  const profiles=[
+    {name:"Tato",age:10},
+    {name:"Hanako",age:100},
+    {name:"noname"}
+  ]
+
   return(
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((profile,index)=>{
+          return <User name={profile.name} age={profile.age} key={index}/>
+        })
+      }
     </div>
   )
 }
 
-const Cat=()=>{
-  return <div>にゃお</div>
+const User=(props)=>{
+return <div>Hi,I am {props.name},and {props.age} years old!</div>
 }
+
+User.defaultProps={
+  age:1
+}
+
 export default App;
 
