53bc1beb04ca736e77e74574f7cb70628d8a85da
yasuyasu-w <ya.su.ya.su.w0612@gmail.com>
2020-04-15 06:18:00 +0000

    implement event new component

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,18 @@
 import axios from 'axios'
 export const READ_EVENTS = 'READ_EVENTS'
+export const CREATE_EVENTS = 'CREATE_EVENTS'
 
 const ROOT_URL='https://udemy-utils.herokuapp.com/api/v1'
 const QUERYSTRING='?token=token123'
 
-
+//最初の表示のアクション
 export const readEvents = () =>async dispatch => {
     const response=await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
     dispatch({type:READ_EVENTS,response})
 }
 
+//サブミットアクション
+export const postEvent = values =>async dispatch => {
+    const response=await axios.post(`${ROOT_URL}/events${QUERYSTRING}`,values)
+    dispatch({type:CREATE_EVENTS,response})
+}
